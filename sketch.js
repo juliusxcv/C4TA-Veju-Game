59b7ceb89b3aa000e1b70872f0a79d250d541618
veraschuerch <vera@appswithlove.com>
2023-10-23 16:44:14 +0200

    Veras Änderungen, die wir beim Konflikt gesaved haben.

--- sketch.js
+++ sketch.js
@@ -5,11 +5,12 @@ function setup() {
     createCanvas(windowWidth, windowHeight)
     
     //Loop der die Kreise macht
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < 10; i++){
         let b = new Ball(i)
         balls.push(b)
     }
-
+    timer = new Timer()
+    //player = new Player() // Neu PLAYER IS RETIRED
 }
 
 
@@ -21,11 +22,10 @@ function draw() {
         balls[i].collide() //Vergleicht die Position des Kreises mit den anderen
         balls[i].edges() //Erstellt die Canvasgrenzen
         balls[i].move() //Gibt den Kreisen ihre Gescwindigkeit
-        balls[i].freeze()
         balls[i].show() //Zeichnet die Kreise, die Zahlen sind ihre indexnummern. Die brauchen die Kreise um sich selber zu kennen 
     }  
 
-
+  timer.show()
 
 
 
