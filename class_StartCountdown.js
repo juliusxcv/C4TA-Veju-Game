3b41a2843972ed3e0bd0cf241ea582953418057d
veraschuerch <vera@appswithlove.com>
2023-10-30 11:35:51 +0100

    Update Screen Flow

--- class_StartCountdown.js
+++ class_StartCountdown.js
@@ -1,22 +1,17 @@
 // class_StartCountdown
 
 
-class StartCountdown {
-    constructor() {
-    this.startTime = 3 // startTime = StartCountdown
-}
-
-show() {
+function startCountdown(){
+    startTime = 3 // startTime = StartCountdown
     textAlign(CENTER, CENTER)
     textSize(70)
     noStroke()
-    fill(255,0,0)
-    text(this.startTime, width/2, height-70)
+    fill(255)
+    text(startTime, width/2, height-70)
 
-    if(frameCount % 60 == 0 && this.startTime > 0 ) {
-    this.startTime--
+    if(frameCount % 60 == 0 && startTime > 0 ) {
+        startTime--
     }
-}
 }
     
 
